4270fba6e935b42860c9a96636a3f744b41f2d3a
Roman <isbscompany@gmail.com>
2015-04-20 09:59:23 +0500

    Update init.js

--- templateSwitch/init.js
+++ templateSwitch/init.js
@@ -2,11 +2,11 @@ if (Meteor.isClient) {
   Restaurants = new Meteor.Collection;
 
   Meteor.startup(function() {
-    Restaurants.insert({name: "Chinese"});
-    Restaurants.insert({name: "Thai"});
-    Restaurants.insert({name: "Japanese"});
-    Restaurants.insert({name: "Indonesian"});
-    Restaurants.insert({name: "Italian"});
+    Restaurants.insert({name: "Chinese", likes: 40, phone: "+015555555"});
+    Restaurants.insert({name: "Thai", likes: 20, phone: "+015555555"});
+    Restaurants.insert({name: "Japanese", likes: 430, phone: "+015555555"});
+    Restaurants.insert({name: "Indonesian", likes: 50, phone: "+015555555"});
+    Restaurants.insert({name: "Italian", likes: 70, phone: "+015555555"});
 
   });
 }
